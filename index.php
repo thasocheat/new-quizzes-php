8b3d07518ac6039903d0d9caec8634feaab86a69
thasocheat <96945084+thasocheat@users.noreply.github.com>
2022-08-07 08:23:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -43,39 +43,6 @@
 <body>
   
 
-<nav class="navbar navbar-expand-lg bg-light fixed-to">
-  <div class="container-fluid hello">
-    <span>Hello🖐<span style="color: red;"> <?php echo $user_data['user_name']; ?></span></span>
-
-    
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-auto">
-        <li class="nav-item">
-        <!-- <span>រយះពេលរបស់អ្នកមាន ៖ <span id="demo"></span></span> -->
-          <!-- <a class="nav-link active" aria-current="page" href="#">Home</a> -->
-        </li>
-        <!-- <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li> -->
-        
-       
-      </ul>
-
-      <div class="button-logout">
-        
-        <a href="forms/sign_upt.php" class="btn btn-info">Log Out</a>
-      </div>
-     
-    </div>
-    
-  </div>
-  
-</nav>
-
-
 
   Hello
   
