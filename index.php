89cdf0d5e3176bda81982cda6d184470dab75775
thasocheat <96945084+thasocheat@users.noreply.github.com>
2022-08-07 08:47:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
- <!-- CSS only -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ស្វាគមន៍មកកាន់ការឆ្លើយសំណួរ</title>
+    <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 
     <!-- Link Style -->
-    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="../style/style.css">
     <link rel="stylesheet" href="style/navbar.css">
     
 
@@ -19,42 +26,85 @@
         }
     </style>
 
-<?php 
-  // session_start();
-  require_once ('database/connection.php');
-  // require_once ('function/function.php');
-  
-	// $user_data = check_login($con);
 
-?>
-<nav class="navbar navbar-expand-lg bg-light fixed-to">
-  <div class="container-fluid hello">
-    <span>Hello🖐<span style="color: red;"> <?php echo $user_data['user_name']; ?></span></span>
 
-    
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-auto">
-        <li class="nav-item">
-        <!-- <span>រយះពេលរបស់អ្នកមាន ៖ <span id="demo"></span></span> -->
-          <!-- <a class="nav-link active" aria-current="page" href="#">Home</a> -->
-        </li>
-        <!-- <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li> -->
-        
-       
-      </ul>
+</head>
+<body>
+	
+	<style>
+                                
+    .button-login button {
+        border: 1px solid #00A4BD;
+        padding: 10px 25px;
+        text-decoration: none;
+        text-transform: uppercase;
+        font-size: 14px;
+        display: inline-block;
+        color: #00A4BD;
+    }
+    .button-login button:hover {
+        background: #00A4BD;
+        color: #fff;
+    }
+</style>
 
-      <div class="button-logout">
-        
-        <a href="forms/sign_out.php" class="btn btn-info">Log Out</a>
-      </div>
-     
+<div class="conatiner">
+    <div class="row">
+        <div class="col-md-12">
+            <!-- Using Bootstrap Card -->
+            <div class="login-form">
+                <div class="card-form" style="width: 25rem;">
+                    <span class="text-sm text-danger text-center"><?php echo $wrongps; ?></span>
+                    <div class="card-body">
+                        <h2 class="card-title text-center mb-3">Login Form</h2>
+                        <form method="POST">
+                            <div class="field mb-4">
+                                <span class="fa fa-user"></span>
+                                <input type="text" name="user_name" placeholder="Enter Name">
+                            </div>
+                            <div class="field mb-4 space">
+                                <span class="fa fa-lock"></span>
+                                <input type="password" class="pass-key" name="password" placeholder="Password">
+                                <span class="show">SHOW</span>
+                            </div>
+                           
+                            <div class="button-login text-center">
+                                <button type="submit" class="btn p-2 mb-4">Log In</button>
+                            </div>
+                            
+                            <div class="mb-3 form-check">
+                                <p class="text-center">Don't have account?<a href="sign_up.php">Signup Now</a></p>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+               
+            </div>
+        </div>
     </div>
+</div>
+
+	
+   <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+
+    <script>
+      const pass_field = document.querySelector('.pass-key');
+      const showBtn = document.querySelector('.show');
+      showBtn.addEventListener('click', function(){
+       if(pass_field.type === "password"){
+         pass_field.type = "text";
+         showBtn.textContent = "HIDE";
+         showBtn.style.color = "#3498db";
+       }else{
+         pass_field.type = "password";
+         showBtn.textContent = "SHOW";
+         showBtn.style.color = "#222";
+       }
+      });
+    </script>
     
-  </div>
-  
-</nav>
+</body>
+</html>
